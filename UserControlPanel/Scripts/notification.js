aee1b6a200e1e290cb4e9c2bdfac593d3138b768
RoTTeN3013 <130251170+RoTTeN3013@users.noreply.github.com>
2024-05-16 21:51:42 +0200

    Delete UserControlPanel directory

--- UserControlPanel/Scripts/notification.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-    if( $('.message_container').length ) {
-        $('.message_container').addClass("show_msg");
-        setTimeout(() => {
-            $('.message_container').removeClass("show_msg");
-        }, 5000);
-    }
-});
